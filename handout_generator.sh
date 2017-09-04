4c35fa7f756d72b3155dc6921ad3c2d77bd7f17c
Viktoria Nemkin <viktoria.nemkin@gmail.com>
2017-09-04 15:42:21 +0200

    Fixed name.

--- handout_generator.sh
+++ handout_generator.sh
@@ -7,6 +7,10 @@
 
 PDF="$1"
 
+NAME="${1%.pdf}"
+
+echo "Name  "${NAME}
+
 PAGES=$(pdftk "${PDF}" dump_data | grep NumberOfPages | sed 's/NumberOfPages: \([0-9]*\)/\1/')
 SIZEX=$(pdftk "${PDF}" dump_data | grep PageMediaDimensions | head -n 1 | sed 's/PageMediaDimensions: \([0-9]*\) \([0-9]*\)/\1/')
 SIZEY=$(pdftk "${PDF}" dump_data | grep PageMediaDimensions | head -n 1 | sed 's/PageMediaDimensions: \([0-9]*\) \([0-9]*\)/\2/')
@@ -23,6 +27,7 @@ pdftk A="${PDF}" B=blank.pdf cat ${COMMAND} output combined.pdf
 
 # convert combined.pdf -bordercolor black -border 10 with_border.pdf
 
-pdfjam combined.pdf --offset '2cm 0cm' --nup 2x3 --a4paper --no-landscape --frame true --outfile result.pdf
+pdfjam combined.pdf --offset '2cm 0cm' --nup 2x3 --a4paper --no-landscape --frame true --outfile ${NAME}_handout.pdf
 
 rm blank.pdf
+rm combined.pdf
